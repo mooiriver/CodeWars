5b6897237d8b5fe655ae93943c7c71fe1c12b252
mooiriver <33868303+mooiriver@users.noreply.github.com>
2017-11-21 15:35:25 +0200

    Create BitCountingTest.cs

---
+++ BitCountingTest.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class BitCounting
+{
+  [Test]
+  public void CountBits()
+  {
+    Assert.AreEqual(0, Kata.CountBits(0));
+    Assert.AreEqual(1, Kata.CountBits(4));
+    Assert.AreEqual(3, Kata.CountBits(7));
+    Assert.AreEqual(2, Kata.CountBits(9));
+    Assert.AreEqual(2, Kata.CountBits(10));
+  }
+}
